21c0cbde856a4dfdf2a7a3aea09f5ea9984f3cdb
Topscientist <74202229+Topscientist@users.noreply.github.com>
2023-07-07 21:52:23 +0000

    Will get this working tommorow

--- src/vm/vm.nim
+++ src/vm/vm.nim
@@ -1,22 +1,19 @@
 #[
 Nim VM, Virtual machine for compiling and interepreting the hanual bytecode. It is speedy as nim is complied to C.
 !! DO NOT CHANGE THIS UNLESS YOU KNOW WHAT YOU'RE DOING !!
-because I definitely do.
-
-        # Notes on bytecode
-
-     
+== CONFIGUARTION FOR THE VM CAN BE FOUND IN vm.conig ==
 
         # File extension (Not related to chernobal)
         .chnl
 
-        # Docs links
+        # Docs links & References
         https://nim-lang.org/docs/manual.html#exception-handling
         https://nim-lang.org/docs/manual.html#statements-and-expressions-while-statement
         https://nim-lang.org/docs/os.html
         https://nim-lang.org/docs/tut2.html#exceptions-raise-statement
         https://stackoverflow.com/questions/34427858/reading-bytes-from-many-files-performance
         https://nim-lang.org/docs/io.html
+        https://nim-lang.org/docs/streams.html
 ]#
 
 # Imports
@@ -38,8 +35,9 @@ let R = "" # This register holds the return value of a called function
 
 # Attempt to read the main file
 try:
-  let f = readFile("main.chnl")
-  readBytes(f, 8)
+  let f = open("test.txt")
+  var buf: array[6, byte]
+  f.readBytes(buf, 8)
 
 except CatchableError:
   echo "[VM ERROR]: The main file does not exist, touch the main.chnl file if you are on unix based systems. Thanks bye :]"
